3fcdacafc5e76227b4cc48b22f1f7e430a7bfd12
moestilos <gmateosoficial@gmail.com>
2026-04-16 12:47:33 +0200

    feat: add Supabase credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  supabaseUrl:  process.env['SUPABASE_URL']      ?? '',
-  supabaseKey:  process.env['SUPABASE_ANON_KEY'] ?? '',
+  supabaseUrl:  'https://xzcnaozzmxavbkpytdri.supabase.co',
+  supabaseKey:  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6Y25hb3p6bXhhdmJrcHl0ZHJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzMDQxNTUsImV4cCI6MjA5MTg4MDE1NX0.vQM2AdAdIXSd4eVCrnx1vOijc8Qi6hHfrgE-ejJa1Bc',
   adminEmail:   'gmateosoficial@gmail.com',
 };
